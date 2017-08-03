1e3b1b5c1a7e1283669df217b396b92a7e713e65
CPU10816\CPU10816-local <tuanpn@vng.com.vn>
2017-08-03 11:41:44 +0700

    add store, load to BloomFilter

--- src/main/scala/Benchmark.scala
+++ src/main/scala/Benchmark.scala
@@ -98,11 +98,13 @@ object Benchmark {
 
     def testStaticBloom(train: Array[String], test: Array[String]) = {
         println("Static Bloom Filter")
-//        val bf = new BloomFilter[String](train.length, 0.001)
-        val bf2 = new BloomFilter[String]()
+        val bf1 = new BloomFilter[String](train.length, 0.001)
         val start = System.currentTimeMillis()
-//        bf.add(train.toList)
-        train.foreach(bf2.add(_))
+        bf1.add(train.toList)
+        bf1.store("store.txt")
+
+        val bf = new BloomFilter[String]("store.txt", null)
+
         println("add time: " + (System.currentTimeMillis() - start) / 1000.0)
         val setTrain = train.toSet
         var falsePos = 0
@@ -110,22 +112,20 @@ object Benchmark {
         var countTrue = 0
         var countFalse = 0
 
-//        test.foreach(element => {
-//            if (bf.mightContain(element)) {
-//                countTrue += 1
-//                if (setTrain.contains(element) == false)
-//                    falsePos += 1
-//            }
-//            else {
-//                countFalse += 1
-//                if (setTrain.contains(element))
-//                    falseNeg += 1
-//            }
-//
-//        })
+        test.foreach(element => {
+            if (bf.mightContain(element)) {
+                countTrue += 1
+                if (setTrain.contains(element) == false)
+                    falsePos += 1
+            }
+            else {
+                countFalse += 1
+                if (setTrain.contains(element))
+                    falseNeg += 1
+            }
 
+        })
 
-        println("test 2: " + bf2.mightContain("CpOsDp8pEJ0qDJ4uC38vCZ8mC2unBZOkCJ4tBZPcP30v"))
         println("Total: " + test.length)
         println("faslePos: " + falsePos)
         println("falseNeg: " + falseNeg)
@@ -171,8 +171,8 @@ object Benchmark {
 //        val data = readFile()
         val (train, test) = data.splitAt((data.length / 2).toInt)
 //        val train = data; val test = data
+//        testCoutingBloom(train, test)
         testStaticBloom(train, test)
-        testCoutingBloom(train, test)
-        testScalableBloom(train, test)
+//        testScalableBloom(train, test)
     }
 }
--- src/main/scala/BloomFilter.scala
+++ src/main/scala/BloomFilter.scala
@@ -1,13 +1,20 @@
+import java.io._
+
 import scala.math._
 
-class BloomFilter[T] (numElements: Long = 5000000, probFalsePositive: Double = 0.001, var hashFunction: (T) => Long = null) {
-    private val numBits: Long = ceil((numElements * log(probFalsePositive)) / log(1.0 / (pow(2.0, log(2.0)))))
-    private val numHashs = round(log(2.0) * numBits / numElements)
-    private val numSlices = numBits / 64 + 1
-    private val arrBits = Array.fill(numSlices)(0l)
+class BloomFilter[T] (numElements: Long = 50000000, probFalsePositive: Double = 0.001, var hashFunction: (T) => Long = null) {
+    private var numBits: Long = ceil((numElements * log(probFalsePositive)) / log(1.0 / (pow(2.0, log(2.0)))))
+    private var numHashes = round(log(2.0) * numBits / numElements)
+    private var numSlices = numBits / 64 + 1
+    private var arrBits = Array.fill(numSlices)(0l)
 
     if (hashFunction == null)
         hashFunction = CustomHash_x64
+
+    def this(fileName: String, hashFunction: (T) => Long) {
+        this(1, 0, hashFunction)
+        load(fileName, hashFunction)
+    }
 
     implicit def longToInt(l: Long): Int = {
         return l.toInt
@@ -42,13 +49,11 @@ class BloomFilter[T] (numElements: Long = 5000000, probFalsePositive: Double = 0
     }
 
     def add(element: T): Unit = {
-        if (element.toString == "CpOsDp8pEJ0qDJ4uC38vCZ8mC2unBZOkCJ4tBZPcP30v")
-            println("add")
         val hash = hashFunction(element)
         val hash1 = hash >>> 32
         val hash2 = (hash << 32) >> 32
 
-        for (i <- 0 to numHashs) {
+        for (i <- 0 to numHashes) {
             val computedHash = hash1 + i * hash2
             setBit((computedHash & Long.MaxValue) % numBits)
         }
@@ -63,20 +68,81 @@ class BloomFilter[T] (numElements: Long = 5000000, probFalsePositive: Double = 0
         val hash1 = hash >>> 32
         val hash2 = (hash << 32) >> 32
 
-        for (i <- 0 to numHashs) {
+        for (i <- 0 to numHashes) {
             val computedHash = hash1 + i * hash2
             if (!getBit((computedHash & Long.MaxValue) % numBits))
                 return false
         }
         return true
     }
+    
+    def store(fileName: String) = {
+        val start = System.currentTimeMillis()
+        val pw = new PrintWriter(new File(fileName))
+        pw.println(numBits)
+        pw.println(numHashes)
+        pw.println(numSlices)
+        arrBits.foreach(num => {
+            pw.println(num)
+        })
+        pw.close()
+
+        println("Store: " + (System.currentTimeMillis() - start) / 1000.0)
+    }
+
+    def load(fileName: String, _hashFunction: (T) => Long = null) = {
+        val start = System.currentTimeMillis()
+        val stream = new FileInputStream(fileName)
+        val reader = new BufferedReader(new java.io.InputStreamReader(stream))
+        numBits = reader.readLine().toLong
+        numHashes = reader.readLine().toInt
+        numSlices = reader.readLine().toInt
+        arrBits = Array.fill(numSlices)(0L)
+
+        (0 until numSlices).foreach(i => {
+            arrBits(i) = reader.readLine().toLong
+        })
+
+        if (_hashFunction != null)
+            hashFunction = _hashFunction
+        else
+            hashFunction = CustomHash_x64
+        println("Load: " + (System.currentTimeMillis() - start) / 1000.0)
+    }
 }
 
 object Test1 {
     def main(args: Array[String]): Unit = {
-        val cbl = new BloomFilter[Int](700000000, 0.001)
+        val cbl = new BloomFilter[Int](10, 0.01)
         cbl.add(1)
+        cbl.add(2)
+        cbl.add(3)
+        cbl.add(4)
+        cbl.add(5)
+        cbl.add(6)
+        cbl.add(7)
+        cbl.add(8)
+        cbl.add(9)
+        cbl.store("store.txt")
+
+        val bl2 = new BloomFilter[Int]("store.txt", null)
+        println(bl2.mightContain(1))
+        println(bl2.mightContain(2))
+        println(bl2.mightContain(4))
+        println(bl2.mightContain(5))
+        println(bl2.mightContain(7))
+        println(bl2.mightContain(9))
+        println(bl2.mightContain(10))
+        println()
+
+        cbl.load("store.txt")
         println(cbl.mightContain(1))
         println(cbl.mightContain(2))
+        println(cbl.mightContain(4))
+        println(cbl.mightContain(5))
+        println(cbl.mightContain(7))
+        println(cbl.mightContain(9))
+        println(cbl.mightContain(10))
+        println()
     }
 }
